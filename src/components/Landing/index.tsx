9b6a61e7934d956158bebb113a1741feb47a6b94
saur0w <sthapliyal085@gmail.com>
2026-03-20 00:30:06 +0530

    "Landing"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import styles from "./style.module.scss";
+import Scene from "./Scene";
 
 export default function Landing() {
     return (
       <section className={styles.landing}>
-
+        <Scene />
       </section>
     );
 }
