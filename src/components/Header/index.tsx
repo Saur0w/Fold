a1b40911e9fb85c7d2baa735a92339b694c44e4a
saur0w <sthapliyal085@gmail.com>
2026-03-21 12:27:58 +0530

    "Header Import"

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styles from "./style.module.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { SplitText } from "gsap/SplitText";
+
+export default function Header() {
+    return (
+      <header className={styles.header}>
+
+      </header>
+    );
+}
